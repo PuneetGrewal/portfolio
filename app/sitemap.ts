69a965d148afbe94d983b7ea50e6fafb53b227dc
Puneet Grewal <puneetsgrewal@gmail.com>
2025-02-11 20:03:25 -0800

    added file

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,15 +1,12 @@
-export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
+export const baseUrl = 'https://portfolio-blog-starter.vercel.app';
 
-// export default async function sitemap() {
-//   let blogs = getBlogPosts().map((post) => ({
-//     url: `${baseUrl}/blog/${post.slug}`,
-//     lastModified: post.metadata.publishedAt,
-//   }))
+export default async function sitemap() {
+  // Define the routes you want in the sitemap
+  let routes = ['', '/blog'].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString().split('T')[0], // current date in ISO format
+  }));
 
-//   let routes = ['', '/blog'].map((route) => ({
-//     url: `${baseUrl}${route}`,
-//     lastModified: new Date().toISOString().split('T')[0],
-//   }))
-
-//   return [...routes, ...blogs]
-// }
+  // Return the routes
+  return routes;
+}
